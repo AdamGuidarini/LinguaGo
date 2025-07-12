72d3ab8cfb1c57ed274591cf7523cf5674849d0c
Adam Guidarini <45023561+AdamGuidarini@users.noreply.github.com>
2025-07-12 13:35:40 -0600

    Added ng material

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule} from '@angular/material/select';
 
 @Component({
   selector: 'app-search',
   standalone: true,
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule
+  ],
   templateUrl: './search.component.html',
   styleUrl: './search.component.scss'
 })
 export class SearchComponent {
+  constructor() {
 
+  }
 }
