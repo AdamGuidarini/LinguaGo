769b0fa5154220e5e4d06805af4dfc10d8460d4a
Adam Guidarini <adamguidarini@protonmail.com>
2025-07-12 20:23:43 -0600

    Added input fields

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule} from '@angular/material/select';
+import { MatCardModule} from '@angular/material/card';
 
 @Component({
   selector: 'app-search',
@@ -9,7 +10,8 @@ import { MatSelectModule} from '@angular/material/select';
   imports: [
     MatFormFieldModule,
     MatInputModule,
-    MatSelectModule
+    MatSelectModule,
+    MatCardModule
   ],
   templateUrl: './search.component.html',
   styleUrl: './search.component.scss'
