1fc8b5bed0dbee111d58b254780bd33bc71f7fcf
Adam Guidarini <45023561+AdamGuidarini@users.noreply.github.com>
2025-07-13 00:28:35 -0600

    added auto-spies

--- src/app/components/search/search.component.spec.ts
+++ src/app/components/search/search.component.spec.ts
@@ -1,14 +1,15 @@
+import { createSpyFromClass } from 'jest-auto-spies';
 import { LibreTranslateService } from '../../services/libre-translate.service';
 import { SearchComponent } from './search.component';
 
-// const mockLibreTranslateService = createSpyFromClass()
+const mockLibreTranslateService = createSpyFromClass(LibreTranslateService);
 
 describe('SearchComponent', () => {
   let component: SearchComponent;
 
   beforeEach(async () => {
     component = new SearchComponent(
-      // new LibreTranslateService()
+      mockLibreTranslateService
     );
   });
 
